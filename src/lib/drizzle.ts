c329afcd752e662288042c5150f6478a406f841c
mansardauna <olaomansur@gmail.com>
2026-07-18 11:58:20 +0100

    chore: support native Vercel Postgres URL

--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { schema } from './schema';
 
-const databaseUrl = process.env.DATABASE_URL ?? 'postgresql://postgres:password@localhost:5432/postgres';
+const databaseUrl = process.env.POSTGRES_URL || process.env.DATABASE_URL || 'postgresql://postgres:password@localhost:5432/postgres';
 
 const client = postgres(databaseUrl, { prepare: false, ssl: 'require' });
 
